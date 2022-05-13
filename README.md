855b944bcd5e92c02c4ddc216accf7d00f5007f2
Michael Korostelev <korostelevm@gmail.com>
2022-05-13 08:19:59 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# starter-micro-api
+
+This is the simplest possible nodejs api using the base http library that responds to any request with:   
+```ddd
+Yo! 
+```sadf
+asdfasdfdddd
+asdf
+aaaasdfaf
+
+asdfddd
+dsfsadf
+asdfasf
+asdfdddd
+asdf
+asdfdd
---
+++ index.js
@@ -0,0 +1,67 @@
+const path = require('path')
+const express = require('express')
+const app = express()
+
+process.env.CYCLIC_DB = 'glamorous-battledress-tickCyclicDB'
+
+
+
+const CyclicDb = require('cyclic-dynamodb')
+let j = CyclicDb.collection('junk')
+
+
+function sleep(ms) {
+    return new Promise((resolve) => setTimeout(resolve, ms))
+}
+
+app.get('/sleep/:secs', async (req,res)=>{
+    console.log(`sleeping ${req.params.secs}`)
+    for(let i=0; i<=(+req.params.secs); i++){
+        console.log(i)
+        await sleep(1000)
+    }
+    
+    
+    return res.send(`slept ${req.params.secs}`)
+})
+app.all('/file',(req,res)=>{
+    res.sendFile(path.resolve(__dirname, "./image.png"));
+})
+
+app.get('/error', async (req,res)=>{
+    throw 'error'
+})
+
+app.all('/', async (req, res) => {
+    console.log(process.env)
+//    let last_req = await j.get('last_req')
+//    await j.set('last_req', req.query)
+
+    res.cookie(`c`,Buffer.from('yo').toString('base64'), { 
+    domain: 'cyclic-app.com',
+    maxAge: 900000, httpOnly: true });
+    
+    console.log("Just got a request!")
+//     res.statusCode = 401
+//     res.setHeader('WWW-Authenticate','Basic')
+    return res.json({
+        message: 'main bdddranch1'
+    })
+})
+
+app.get('/junk', async (req, res)=>{
+       let last_req = await j.get('last_req')
+       await j.set('last_req', req.headers)
+       let props = last_req.props || {}
+       return res.json({
+           'user-agent': props['user-agent'],
+           updated: props.updated
+           
+       })
+})
+
+app.get('/ifttt', (req, res)=>{
+console.log('yyyy')
+res.status(200).send('ok')
+})
+app.listen(3000)
